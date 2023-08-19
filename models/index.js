1e13b8ee5df7a873b50ad4e185f00e2c34a76297
Viktoriia <freshjs.pd@gmail.com>
2023-08-19 09:43:50 +0300

    Add db connection

---
+++ models/index.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const env = process.env.NODE_ENV ?? 'development';
+
+const { host, port, dbName } = require('./../configs/mongoDBConfigs.json')[env];
+
+mongoose
+  .connect(`mongodb://${host}:${port}/${dbName}`)
+  .then(() => console.log('Connection OK'))
+  .catch(err => console.log('Connection to MongoDB ERROR!!!'));
